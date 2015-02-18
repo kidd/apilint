db899f2fa3fa51bcf52da4c19b8b04e7cbc8f714
Raimon Grau <raimon@3scale.net>
2015-02-18 20:40:23 +0000

    fix regex

--- lib/apilint/request.rb
+++ lib/apilint/request.rb
@@ -1,7 +1,7 @@
 module Apilint
   Request = Struct.new(:host, :uri, :method, :headers) do
     def smart_path
-      smart_path = uri.gsub(%r{(?<=/)([0-9]+)(?=[/.]}, 'param')
+      smart_path = uri.gsub(%r{(?<=/)([0-9]+)(?=[/.])}, 'param')
     end
   end
 end
