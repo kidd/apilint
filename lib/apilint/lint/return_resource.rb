3ee646ca1b7fdb5defe5c383e7bcc6a9beb01951
Raimon Grau <raimon@3scale.net>
2015-02-18 23:47:26 +0000

    remove underscore from _response as we do use it

--- lib/apilint/lint/return_resource.rb
+++ lib/apilint/lint/return_resource.rb
@@ -4,16 +4,16 @@ module Apilint
       MSG = 'You should return the full resource on 200 and 201 but not on 202'
 
 
-      def check(request, _response)
+      def check(request, response)
         format = request.asked_format
-        rules =  { 200 => ->(){ contains_resource(_response, format) }  ,
-                   201 => ->(){ contains_resource(_response, format) }  ,
-                   202 => ->(){ !contains_resource(_response, format)}  ,
+        rules =  { 200 => ->(){ contains_resource(response, format) }  ,
+                   201 => ->(){ contains_resource(response, format) }  ,
+                   202 => ->(){ !contains_resource(response, format)}  ,
                    nil => ->(){ true }
                  }
 
-        unless rules[_response.code].call  #return true if it contains resource
-          add_offense(request.smart_path, _response, :body)
+        unless rules[response.code].call  #return true if it contains resource
+          add_offense(request.smart_path, response, :body)
         end
 
       end
