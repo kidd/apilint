847d9b6804b39ec58bb230d49c6c6b9bef8bdaa1
Toni Reina <areina0@gmail.com>
2015-02-19 00:40:20 +0000

    Add RateLimitStatus lint

---
+++ lib/apilint/lint/rate_limit_status.rb
@@ -0,0 +1,12 @@
+module Apilint
+  module Lint
+    class RateLimitStatus < Linter
+      MSG = 'Return the remaining number of request tokens with each request in the `RateLimit-Remaining` response header.'
+
+      def check(request, response)
+        return if response.headers.any? { |k, v| k =~ /^(X-RateLimit|RateLimit)/ }
+        add_offense(request.smart_path, response, :headers)
+      end
+    end
+  end
+end
