e87446178dfed4140f09a772536fcae37ccfdf9f
Toni Reina <areina0@gmail.com>
2015-02-19 01:23:21 +0000

    Remove Offense#full_message

--- lib/apilint/offense.rb
+++ lib/apilint/offense.rb
@@ -1,7 +1,4 @@
 module Apilint
   Offense = Struct.new(:smart_path, :obj, :attr, :msg) do
-    def full_message
-      "#{obj.class} (#{smart_path}): #{attr} - #{obj.send(attr)} - #{msg}"
-    end
   end
 end
--- lib/apilint/reporter.rb
+++ lib/apilint/reporter.rb
@@ -1,11 +1,14 @@
 module Apilint
   class Reporter
     def report(offenses)
+      # TODO: puts 210 requests inspected, 12285 offenses detected
       if offenses.empty?
         puts "No offenses"
       else
         offenses.each do |offense|
-          puts "* #{offense.full_message}"
+          puts "----"
+          puts "#{offense.smart_path}: #{offense.msg}"
+          puts "\t Offense in #{offense.obj.class} #{offense.attr}. #{offense.obj.send(offense.attr)}"
         end
       end
     end
