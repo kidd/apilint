9484e8708890245bd4a82a5745a485f66cac6e93
Toni Reina <areina0@gmail.com>
2015-02-20 01:23:13 +0000

    UI hacks for reporter

--- lib/apilint/reporter/offense_based.rb
+++ lib/apilint/reporter/offense_based.rb
@@ -12,13 +12,15 @@ module Apilint
 
     def flush
       offenses.group_by{ |o|  o.lint_name }.each do |offense_type , offenses_same_type|
-        puts "----"
-        puts offense_type
+        header = "#{offense_type} - #{offenses_same_type.first.msg}"
+        puts "\n"
+        puts "-" * header.size
+        puts header
+        puts "-" * header.size
         offenses_same_type.group_by{|o| o.smart_path }.each do |offense_smart_path, offenses_same_smart_path|
-          puts "\t----"
-          puts offense_smart_path
+          puts "\n\t - #{offense_smart_path}"
           offenses_same_smart_path.each do |offense|
-            puts "\t\t #{offense.path}: #{offense.attr} : #{offense.obj.send(offense.attr)}"
+            puts "\t\t #{offense.path}: \n\t\tOffense in #{offense.attr}: \"#{offense.obj.send(offense.attr)}\""
           end
         end
       end
